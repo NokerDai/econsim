46572de4ad7d5e49e6a938a3ef107e354fb16ac3
Facundo <facutoitaly@gmail.com>
2026-07-18 04:15:29 -0300

    actualizar streamlit

--- markets/__init__.py
+++ markets/__init__.py
@@ -1,3 +1,4 @@
 from .trabajo import mercado_laboral
 from .productos import mercado_productos
 from .dinero import emisión_monetaria
+from .producción import producción
--- push.bat
+++ push.bat
@@ -4,5 +4,3 @@ cd /d "%~dp0"
 git add .
 git commit -m "actualizar streamlit"
 git push
-
-pause
