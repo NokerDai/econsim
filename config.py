a4c3bcef97bfb53887a3c13506bdc62937b95cc4
Facundo <facutoitaly@gmail.com>
2026-07-18 07:14:37 -0300

    a

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ class Config:
     tasa_emisión: float = 0
 
     # Parámetros del modelo laboral
-    duración_contrato: int = 1
+    duración_contrato: int = 30
     reducción_salario_contratación: float = 0.99
     aumento_salario_vacante: float = 1.01
     informalidad_por_empresa: int = 100
