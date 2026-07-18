2b3add05f6d1f7eb681846cfdbb2337ce072a5d0
Facundo <facutoitaly@gmail.com>
2026-07-18 03:28:53 -0300

    precios

--- markets/productos.py
+++ markets/productos.py
@@ -10,19 +10,8 @@ def mercado_productos(estado):
 
             empresa.presupuesto += empresa.precio
 
-            empresa.ventas_mes += 1
+            empresa.precio *= estado.config.aumento_precio
 
-    if estado.día % estado.config.período_actualización_precios == 0:
+        else:
 
-        for empresa in estado.empresas:
-
-            if empresa.ventas_mes < empresa.ventas_mes_anterior:
-
-                empresa.precio *= estado.config.reducción_precio
-
-            elif empresa.ventas_mes > empresa.ventas_mes_anterior:
-
-                empresa.precio *= estado.config.aumento_precio
-
-            empresa.ventas_mes_anterior = empresa.ventas_mes
-            empresa.ventas_mes = 0
+            empresa.precio *= estado.config.aumento_precio
