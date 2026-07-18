0f9e3cde6fc66f5922f011c602e3b64c9f5a41f3
Facundo <facutoitaly@gmail.com>
2026-07-18 17:09:48 -0300

    fix

--- estadisticas/collector.py
+++ estadisticas/collector.py
@@ -6,20 +6,13 @@ def actualizar_estadisticas(estado):
     precio_medio = mean(e.precio for e in estado.empresas)
     salarios_formales = [empresa.salario for empresa in estado.empresas for empleado in range(empresa.empleados_formales)]
     salarios_informales = [empresa.salario_informal for empresa in estado.empresas for empleado in range(empresa.empleados_informales)]
-    
-    ajuste_formal = 0
-    ajuste_informal = 0
-    if len(estado.estadisticas.salario_medio) > 0:
-        formal = estado.estadisticas.salario_medio[-1]
-    if len(estado.estadisticas.salario_informal_medio) > 0:
-        informal = estado.estadisticas.salario_informal_medio[-1]
 
     estado.estadisticas.salario_medio.append(
-        mean(salarios_formales) if salarios_formales else ajuste_formal
+        mean(salarios_formales) if salarios_formales else 0.0
     )
 
     estado.estadisticas.salario_informal_medio.append(
-        mean(salarios_informales) if salarios_informales else ajuste_informal
+        mean(salarios_informales) if salarios_informales else 0.0
     )
 
     estado.estadisticas.precio_medio.append(
