ec0427ad99448ce825bc81a826c9718aa7ec8bb3
Facundo <facutoitaly@gmail.com>
2026-07-18 05:42:18 -0300

    actualizar streamlit

--- markets/productos.py
+++ markets/productos.py
@@ -11,7 +11,7 @@ def mercado_productos(estado):
     if estado.día % estado.config.intervalo_ajuste == 0:
         for empresa in estado.empresas:
             if empresa.ventas < empresa.ventas_anteriores:
-                empresa.precio *= estado.config.reducción_precio ** (empresa.ventas_anteriores - empresas.ventas)
+                empresa.precio *= estado.config.reducción_precio ** (empresa.ventas_anteriores - empresa.ventas)
             elif empresa.ventas > empresa.ventas_anteriores:
                 empresa.precio *= estado.config.aumento_precio ** (empresa.ventas - empresa.ventas_anteriores)
             empresa.ventas_anteriores = empresa.ventas
