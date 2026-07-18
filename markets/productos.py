6abf0cb2102582d0c78a24ea3c74b24fede25259
Facundo <facutoitaly@gmail.com>
2026-07-18 08:18:55 -0300

    a

--- markets/productos.py
+++ markets/productos.py
@@ -1,17 +1,20 @@
 def mercado_productos(estado):
 
+    empresas = estado.empresas
+    aleatorio = estado.aleatorio
+    aumento = estado.config.aumento_precio
+    reduccion = estado.config.reducción_precio
+
     for trabajador in estado.trabajadores:
 
-        empresa = estado.aleatorio.choice(estado.empresas)
+        empresa = aleatorio.choice(empresas)
 
         if trabajador.presupuesto >= empresa.precio:
+            precio = empresa.precio
 
-            trabajador.presupuesto -= empresa.precio
-
-            empresa.presupuesto += empresa.precio
-
-            empresa.precio *= estado.config.aumento_precio
+            trabajador.presupuesto -= precio
+            empresa.presupuesto += precio
+            empresa.precio = precio * aumento
 
         else:
-            
-            empresa.precio *= estado.config.reducción_precio
+            empresa.precio *= reduccion
