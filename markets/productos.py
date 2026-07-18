74334b8d9fe3cf3b61b964771266c3b8d23b81e9
Facundo <facutoitaly@gmail.com>
2026-07-18 08:54:52 -0300

    simple

--- markets/productos.py
+++ markets/productos.py
@@ -1,21 +1,12 @@
 def mercado_productos(estado):
 
-    productos_disponibles = [empresa for empresa in estado.empresas for vacante in range(10)]
-    productos_disponibles.sort(key=lambda e: e.precio)
-
     for trabajador in estado.trabajadores:
-        if not productos_disponibles:
-            break
-
-        empresa = productos_disponibles.pop(0)
+        empresa = estado.aleatorio.choice(estado.empresas)
 
         if trabajador.presupuesto >= empresa.precio:
             trabajador.presupuesto -= empresa.precio
             empresa.presupuesto += empresa.precio
             empresa.precio *= estado.config.aumento_precio
-            empresa.vendió = True
 
-    for empresa in estado.empresas:
-        if not empresa.vendió:
+        else:
             empresa.precio *= estado.config.reducción_precio
-        empresa.vendió = False
--- markets/trabajo.py
+++ markets/trabajo.py
@@ -1,20 +1,17 @@
 def mercado_laboral(estado):
 
     vacantes_formales = [empresa for empresa in estado.empresas for vacante in range(int(empresa.presupuesto / empresa.salario))]
-    vacantes_formales.sort(key=lambda e: e.salario, reverse=True)
+    estado.aleatorio.shuffle(vacantes_formales)
 
     for trabajador in estado.trabajadores:
         if not vacantes_formales:
             break
 
-        empresa = vacantes_formales.pop(0)
+        empresa = estado.aleatorio.pop(0)
 
         trabajador.presupuesto += empresa.salario
         empresa.presupuesto -= empresa.salario
         empresa.salario *= estado.config.reducción_salario
-        empresa.contrató = True
 
     for empresa in estado.empresas:
-        if not empresa.contrató:
-            empresa.salario *= estado.config.aumento_salario
-        empresa.contrató = False
+        empresa.salario *= estado.config.aumento_salario
