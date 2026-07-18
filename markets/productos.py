d57ea0f7fe36fb25124d1558eea36eeb61065fbe
Facundo <facutoitaly@gmail.com>
2026-07-18 08:31:32 -0300

    fix

--- markets/productos.py
+++ markets/productos.py
@@ -9,12 +9,13 @@ def mercado_productos(estado):
         i = estado.aleatorio.randrange(len(productos_disponibles))
         empresa = productos_disponibles[i]
 
-        trabajador.presupuesto -= empresa.precio
-        empresa.presupuesto += empresa.precio
-        empresa.precio *= estado.config.aumento_precio
+        if trabajador.presupuesto >= empresa.precio:
+            trabajador.presupuesto -= empresa.precio
+            empresa.presupuesto += empresa.precio
+            empresa.precio *= estado.config.aumento_precio
 
-        productos_disponibles[i] = productos_disponibles[-1]
-        productos_disponibles.pop()
+            productos_disponibles[i] = productos_disponibles[-1]
+            productos_disponibles.pop()
 
     for empresa in productos_disponibles:
         empresa.precio *= estado.config.reducción_precio
