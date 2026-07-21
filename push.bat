ec5a746b44c3bdc3cb592e286b558334f8f3aefd
Facundo <facutoitaly@gmail.com>
2026-07-21 06:57:39 -0300

    a

--- push.bat
+++ push.bat
@@ -3,6 +3,14 @@ cd /d "%~dp0"
 
 set /p mensaje=Escribí el mensaje del commit:
 
+echo === git add ===
 git add .
+pause
+
+echo === git commit ===
 git commit -m "%mensaje%"
-echo n | git push
+pause
+
+echo === git push ===
+git push
+pause
