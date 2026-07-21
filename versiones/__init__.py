3ef74db4501d0a592147439dc2f0f6f798c806c5
Facundo <facutoitaly@gmail.com>
2026-07-21 16:01:28 -0300

    a

--- versiones/__init__.py
+++ versiones/__init__.py
@@ -6,4 +6,4 @@ _ruta_actual = os.path.dirname(__file__)
 VERSIONES_DISPONIBLES = sorted([
     nombre for nombre in os.listdir(_ruta_actual)
     if os.path.isdir(os.path.join(_ruta_actual, nombre)) and not nombre.startswith("__")
-])
+], reverse=True)
