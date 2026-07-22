5002e868d5dc4f5a6409d575096915607561b98a
Facundo <facutoitaly@gmail.com>
2026-07-22 17:20:28 -0300

    M0 total en vez de M0 per cápita

--- versiones/v3/dinero.py
+++ versiones/v3/dinero.py
@@ -2,8 +2,8 @@ def emisión_monetaria(estado):
 
     tasa = 0
     if estado.config.mantenimiento_M0 and estado.config.tasa_emisión == 0:
-        M0_pc_inicial = (estado.config.presupuesto_inicial * estado.config.num_empresas) / estado.config.num_trabajadores
-        M0_pc_actual = (sum([e.presupuesto for e in estado.empresas]) + sum([t.presupuesto for t in estado.trabajadores])) / len(estado.trabajadores)
+        M0_pc_inicial = estado.config.presupuesto_inicial * estado.config.num_empresas
+        M0_pc_actual = sum([e.presupuesto for e in estado.empresas]) + sum([t.presupuesto for t in estado.trabajadores])
         tasa = (M0_pc_inicial / M0_pc_actual + estado.config.mantenimiento_M0_suavizado) / (estado.config.mantenimiento_M0_suavizado + 1)
     else:
         tasa = 1 + estado.config.tasa_emisión
