afacec9ee335f4a931a7b57014141bc4bd9128e7
Facundo <facutoitaly@gmail.com>
2026-07-23 05:46:23 -0300

    era endógena no exógena, M0 tiene mínimo la inicial

--- versiones/v3/dinero.py
+++ versiones/v3/dinero.py
@@ -1,10 +1,11 @@
 def emisión_monetaria(estado):
 
-    tasa = 0
+    tasa = 1
     if estado.config.mantenimiento_M0 and estado.config.tasa_emisión == 0:
         M0_pc_inicial = estado.config.presupuesto_inicial * estado.config.num_empresas
         M0_pc_actual = sum([e.presupuesto for e in estado.empresas]) + sum([t.presupuesto for t in estado.trabajadores])
-        tasa = (M0_pc_inicial / M0_pc_actual + estado.config.mantenimiento_M0_suavizado) / (estado.config.mantenimiento_M0_suavizado + 1)
+        if M0_pc_actual < M0_pc_inicial:
+            tasa = (M0_pc_inicial / M0_pc_actual + estado.config.mantenimiento_M0_suavizado) / (estado.config.mantenimiento_M0_suavizado + 1)
     else:
         tasa = 1 + estado.config.tasa_emisión
 
