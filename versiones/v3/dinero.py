3aa9c18758efc3b93d0ed492c9464871f0219b61
Facundo <facutoitaly@gmail.com>
2026-07-23 09:09:25 -0300

    a

--- versiones/v3/dinero.py
+++ versiones/v3/dinero.py
@@ -2,10 +2,14 @@ def emisión_monetaria(estado):
 
     tasa = 1
     if estado.config.mantenimiento_M0 and estado.config.tasa_emisión == 0:
-        M0_pc_inicial = estado.config.presupuesto_inicial * estado.config.num_empresas
-        M0_pc_actual = sum([e.presupuesto for e in estado.empresas]) + sum([t.presupuesto for t in estado.trabajadores])
-        if M0_pc_actual < M0_pc_inicial:
-            tasa = (M0_pc_inicial / M0_pc_actual + estado.config.mantenimiento_M0_suavizado) / (estado.config.mantenimiento_M0_suavizado + 1)
+        num_empresas = estado.config.num_empresas
+        num_trabajadores = estado.config.num_trabajadores
+
+        M0_inicial = estado.config.presupuesto_inicial * num_empresas
+        M0_pt_inicial = M0_inicial / num_trabajadores
+        M0_actual_pt = sum(t.presupuesto for t in estado.trabajadores) / len(estado.trabajadores)
+
+        tasa = (M0_pt_inicial / M0_actual_pt + estado.config.mantenimiento_M0_suavizado) / (estado.config.mantenimiento_M0_suavizado + 1)
     else:
         tasa = 1 + estado.config.tasa_emisión
 
