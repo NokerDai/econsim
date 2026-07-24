84247927876cda5d73d361fac7797b98b4a3de38
Facundo <facutoitaly@gmail.com>
2026-07-24 09:44:52 -0300

    a

--- versiones/v3/dinero.py
+++ versiones/v3/dinero.py
@@ -7,10 +7,15 @@ def emisión_monetaria(estado):
 
         M0_inicial = estado.config.presupuesto_inicial * num_empresas
         M0_pt_inicial = M0_inicial / num_trabajadores
+        M0_pe_inicial = estado.config.presupuesto_inicial
+        i = M0_pt_inicial + M0_pe_inicial
+
         M0_actual = sum(t.presupuesto for t in estado.trabajadores) + sum(e.presupuesto for e in estado.empresas)
         M0_pt_actual = M0_actual / len(estado.trabajadores)
+        M0_pe_actual = M0_actual / len(estado.empresas)
+        a = M0_pt_actual + M0_pe_actual
 
-        tasa = max(1, (M0_pt_inicial / M0_pt_actual + estado.config.mantenimiento_M0_suavizado) / (estado.config.mantenimiento_M0_suavizado + 1))
+        tasa = max(1, (i / a + estado.config.mantenimiento_M0_suavizado) / (estado.config.mantenimiento_M0_suavizado + 1))
     else:
         tasa = 1 + estado.config.tasa_emisión
 
