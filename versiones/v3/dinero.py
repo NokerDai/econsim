1acdd6ff3a0cc169f1324f985aca4ec867990a19
Facundo <facutoitaly@gmail.com>
2026-07-24 09:56:29 -0300

    a

--- versiones/v3/dinero.py
+++ versiones/v3/dinero.py
@@ -1,21 +1,34 @@
 def emisión_monetaria(estado):
+    config = estado.config
 
     tasa = 1
-    if estado.config.mantenimiento_M0 and estado.config.tasa_emisión == 0:
+    if config.mantenimiento_M0 and estado.config.tasa_emisión == 0:
+        peso_trabajadores = config.mantenimiento_M0_peso_trabajadores
+
         num_empresas = estado.config.num_empresas
         num_trabajadores = estado.config.num_trabajadores
 
         M0_inicial = estado.config.presupuesto_inicial * num_empresas
         M0_pt_inicial = M0_inicial / num_trabajadores
         M0_pe_inicial = estado.config.presupuesto_inicial
-        i = M0_pt_inicial + M0_pe_inicial
 
         M0_actual = sum(t.presupuesto for t in estado.trabajadores) + sum(e.presupuesto for e in estado.empresas)
         M0_pt_actual = M0_actual / len(estado.trabajadores)
         M0_pe_actual = M0_actual / len(estado.empresas)
-        a = M0_pt_actual + M0_pe_actual
 
-        tasa = max(1, (i / a + estado.config.mantenimiento_M0_suavizado) / (estado.config.mantenimiento_M0_suavizado + 1))
+        objetivo = (
+            peso_trabajadores * M0_pt_inicial +
+            (1 - peso_trabajadores) * M0_pe_inicial
+        )
+
+        actual = (
+            peso_trabajadores * M0_pt_actual +
+            (1 - peso_trabajadores) * M0_pe_actual
+        )
+
+        tasa = objetivo / actual
+
+        tasa = max(1, (objetivo / actual + estado.config.mantenimiento_M0_suavizado) / (estado.config.mantenimiento_M0_suavizado + 1))
     else:
         tasa = 1 + estado.config.tasa_emisión
 
