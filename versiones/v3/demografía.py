9cce55b1b18560b9d290f1da9edbe6c292dd10ee
Facundo <facutoitaly@gmail.com>
2026-07-24 08:44:41 -0300

    a

--- versiones/v3/demografía.py
+++ versiones/v3/demografía.py
@@ -38,7 +38,7 @@ def demografía_y_firmas(estado):
         e for e in estado.empresas
         if not (
             (e.presupuesto < e.salario_informal and e.inventario == 0)
-            or (e.días_sin_vender > 180 and rand.random() < 0.10)
+            or (e.días_sin_vender > 30 and rand.random() < 0.10)
         )
     ]
 
